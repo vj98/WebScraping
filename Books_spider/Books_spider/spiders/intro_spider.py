8a9ce64912ad5eff461a09602f3ecb0358113730
Vijay Bhati <bhativijay17@gmail.com>
2019-01-06 16:26:30 +0530

    Update intro_spider.py

--- Books_spider/Books_spider/spiders/intro_spider.py
+++ Books_spider/Books_spider/spiders/intro_spider.py
@@ -1,25 +1,21 @@
 import scrapy
 
-filename = "book_titles.txt"
+filename = "book_titles.txt"  # To save store data
 
 class IntroSpider(scrapy.Spider):
-    name = "intro_spider"
+    name = "intro_spider"     # Name of the scraper
 
     def start_requests(self):
         urls = [
-            'http://books.toscrape.com/catalogue/page-1.html',
-            'http://books.toscrape.com/catalogue/page-2.html',
-            'http://books.toscrape.com/catalogue/page-3.html',
-            'http://books.toscrape.com/catalogue/page-4.html',
-            'http://books.toscrape.com/catalogue/page-5.html'
+            'http://books.toscrape.com/catalogue/page-{x}.html'..format(x=x) for x in range(1, 10)   # x denotes page number
         ]
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
     
     def parse(self, response):
-        book_list = response.css('article.product_pod > h3 > a::attr(title)').extract()
+        book_list = response.css('article.product_pod > h3 > a::attr(title)').extract()  # accessing the titles
 
-        with open(filename, 'a+') as f:
+        with open(filename, 'a+') as f:   # Writing data in the file
             for book_title in book_list:
                 f.write(book_title + "\n")
